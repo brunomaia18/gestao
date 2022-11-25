90f0fb9fc0ac809e46718b1f2df3754cf92bfa1c
brunomaia18 <bruninho.maia001840@gmail.com>
2022-11-25 08:42:17 -0300

    Pagina dashboard com grafico

--- dashboard/grafico.php
+++ dashboard/grafico.php
@@ -1,8 +1,3 @@
-
-
-
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -13,12 +8,6 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 </head>
 <body>
-    <div class="text-center">
-        <input type="number" name="month" id="month">
-        <button name='buscar'>Buscar</button>
-    </div>
-    <br>
-    <br>
 <?php
 include "../config.php";
 //---------------------NUMERO DE CLIENTES CADSTRADO NA LOJA -----------------------------//
@@ -28,12 +17,28 @@ $n = $Nclientes->fetchObject();
 //--------------------FIM DO CODIGO-----------------------------------------------------//
 
 //--------------------TOTAL VENDIDO NO MES----------------------------------------------//
-
-$totalVendido = $conn->prepare("SELECT SUM(valor) as valor From cadastro WHERE MONTH(venda) ");
+$totalVendido = $conn->prepare("SELECT SUM(valor) as valor From cadastro  ");
 $totalVendido->execute();
 $valorSoma = $totalVendido->fetchObject();
+
 //--------------------FIM DO CODIGO-----------------------------------------------------//
+
+//----------------------------------------Primeiro Lugar do Bairro que Mais Vende---------------------------------------------------
+$quer_cont1 = $conn->prepare("SELECT bairro as bairros1,COUNT(bairro) as qntvez1  FROM cadastro GROUP BY bairro HAVING COUNT(bairro) >= 1 ORDER BY count(bairro) DESC LIMIT 1");
+$quer_cont1->execute();
+$valor1 = $quer_cont1->fetchObject();
+//--------------------------------------------FIM---------------------------------------------------------------------------------
+
+//-----------------------------------------Segundo Lugar Do Bairro que mais vende-------------------------------------------------
+$quer_cont2 = $conn->prepare("SELECT bairro as bairros2,COUNT(bairro) as qntvez2  FROM cadastro GROUP BY bairro HAVING COUNT(bairro) >= 1 ORDER BY count(bairro) DESC LIMIT 2");
+$quer_cont2->execute();
+while($valor2 = $quer_cont2->fetchObject()){
+    $bairros2 = $valor2->bairros2;
+    $qntvez2 = $valor2->qntvez2;
+};
+//--------------------------------------------FIM---------------------------------------------------------------------------------
 ?>
+
 <div class="container text-center">
 <div class="row">
                     <div class="col-xl-3 col-lg-6 col-md-6 col-sm-12 col-12">
@@ -41,7 +46,8 @@ $valorSoma = $totalVendido->fetchObject();
                             <div class="card-body">
                                 <div class="d-inline-block">
                                     <h5 class="text-muted">VALOR VENDIDO</h5>
-                                    <h2 class="mb-0"><?php echo $valorSoma->valor; ?></h2>
+                                    <h2 class="mb-0"><?php echo "R$ ".$valorSoma->valor;  ; ?></h2>
+                                    
                                 </div>
                                 <div class="float-right icon-circle-medium  icon-box-lg  bg-primary-light mt-1">
                                     <i class="fa fa-user fa-fw fa-sm text-primary"></i>
@@ -54,7 +60,7 @@ $valorSoma = $totalVendido->fetchObject();
                                                 <div class="card-body">
                                                     <div class="d-inline-block">
                                                         <h5 class="text-muted">Clientes Cadastrado</h5>
-                                                        <h2 class="mb-0"><?php echo $n->clientes; ?></h2>
+                                                        <h2 class="mb-0"><?php echo $n->clientes ; ?></h2>
                                                     </div>
                                                     <div class="float-right icon-circle-medium  icon-box-lg  bg-info-light mt-1">
                                                         <i class="fa fa-users fa-fw fa-sm text-info"></i>
@@ -67,7 +73,7 @@ $valorSoma = $totalVendido->fetchObject();
                                                                 <div class="card-body">
                                                                     <div class="d-inline-block">
                                                                         <h5 class="text-muted">Pedidos</h5>
-                                                                        <h2 class="mb-0"></h2>
+                                                                        <h2 class="mb-0">loading...</h2>
                                                                     </div>
                                                                     <div class="float-right icon-circle-medium  icon-box-lg  bg-success-light mt-1">
                                                                         <i class=" fas fa-cart-plus fa-fw fa-sm text-success"></i>
@@ -75,7 +81,47 @@ $valorSoma = $totalVendido->fetchObject();
                                                                 </div>
                                                             </div>
                                                         </div>
+                                                        
 </div>
 </div>
+
+<script src="https://code.jquery.com/jquery-3.6.1.js" integrity="sha256-3zlB5s2uwoUzrXK3BT7AX3FyvojsraNFxCc2vC/7pNI=" crossorigin="anonymous"></script>
+<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"> </script>
+  <script type="text/javascript">
+  
+  google.charts.load("current", {packages:['corechart']});
+    google.charts.setOnLoadCallback(drawChart);
+    function drawChart() {
+      var data = google.visualization.arrayToDataTable([
+        ["Element", "Density", { role: "style" } ],
+        ['<?php echo $valor1->bairros1 ?>', <?php echo $valor1->qntvez1 ?>, "#b87333"],
+        ["<?php echo  $bairros2 ?>",  <?php echo $qntvez2 ?>, "gold"],
+        ["oi", 1, "color: #e5e4e2"]
+      ]);
+
+      var view = new google.visualization.DataView(data);
+      view.setColumns([0, 1,
+                       { calc: "stringify",
+                         sourceColumn: 1,
+                         type: "string",
+                         role: "annotation" },
+                       2]);
+
+      var options = {
+        title: "Numeros de entregas em cada Bairro",
+        width: 600,
+        height: 400,
+        bar: {groupWidth: "95%"},
+        legend: { position: "none" },
+      };
+      var chart = new google.visualization.ColumnChart(document.getElementById("columnchart_values"));
+      chart.draw(view, options);
+  }
+
+ 
+  </script>
+<div id="columnchart_values" style="width: 900px; height: 300px;"></div>
+
+
 </body>
 </html>
